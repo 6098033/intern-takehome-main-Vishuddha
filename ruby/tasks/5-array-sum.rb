84153dc47c885ca1d3681e4b2b475f8884b8d2c9
vishuddha vithanage <dvishuddha@gmail.com>
2022-03-03 13:54:59 +1100

    array sum

--- ruby/tasks/5-array-sum.rb
+++ ruby/tasks/5-array-sum.rb
@@ -6,6 +6,12 @@
  # @returns {Number} The sum of all integers contained in the input, at any level.
 #
 def array_sum(start_of_tree)
-
+ array1 = start_of_tree.select {|element| element.class == Fixnum}
+  if array1.length == start_of_tree.length
+   return start_of_tree.sum
+  elsif
+   array = start_of_tree.flatten!
+   int_array = array.select {|element| element.class == Fixnum}
+   return int_array.sum
+  end
 end
-
